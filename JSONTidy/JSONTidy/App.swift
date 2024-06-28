1a17303f61854af88b6189bc96653017215300fa
Safiya Yunaeva <safiya.yunaeva@gmail.com>
2024-06-27 22:37:44 -0700

    Swift: MacOS app to parse json files

---
+++ JSONTidy/JSONTidy/App.swift
@@ -0,0 +1,16 @@
+//
+//  App.swift
+//  JSONTidy
+//
+//  Created by syunaeva on 6/27/24.
+//
+
+import Foundation
+import ArgumentParser
+
+@main
+struct App: ParsableCommand {
+    func run() {
+        
+    }
+}
